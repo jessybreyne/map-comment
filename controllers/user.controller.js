1780018f406637ce378982ed11c5959eaa212e16
Jessy BREYNE <jessybreyne@protonmail.com>
2021-06-28 15:03:00 +0200

    Ménage dans le controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,4 @@
 const UserModel = require('../models/user.model');
-const ObjectID = require('mongoose').Types.ObjectId;
 
 module.exports.getAllUsers = async (req, res) => {
     // select all users without password
@@ -47,66 +46,6 @@ module.exports.deleteUser = async (req, res) => {
     try {
         await UserModel.remove({ _id: req.params.id }).exec();
         res.status(200).json({ message: "Successfully deleted." });
-    } catch (err) {
-        return res.status(500).json({ message: err });
-    }
-};
-
-module.exports.follow = async (req, res) => {
-    if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToFollow))
-        return res.status(400).send('ID unknow : ' + req.params.id + ' or ' + req.body.idToFollow);
-
-    try {
-        // add to the follower list
-        await UserModel.findByIdAndUpdate(
-            req.params.id,
-            { $addToSet: { following: req.body.idToFollow } },
-            { new: true, upsert: true },
-            (err, docs) => {
-                if (!err) res.status(201).json(docs);
-                else return res.status(400).json(err);
-            }
-        );
-        // add to following list
-        await UserModel.findByIdAndUpdate(
-            req.body.idToFollow,
-            { $addToSet: { followers: req.params.id } },
-            { new: true, upsert: true },
-            (err, docs) => {
-                // if(!err) res.status(201).json(docs);
-                if (err) return res.status(400).json(err);
-            }
-        )
-    } catch (err) {
-        return res.status(500).json({ message: err });
-    }
-};
-
-module.exports.unfollow = async (req, res) => {
-    if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToUnfollow))
-        return res.status(400).send('ID unknow : ' + req.params.id + ' or ' + req.body.idToUnfollow);
-
-    try {
-        // remove to the follower list
-        await UserModel.findByIdAndUpdate(
-            req.params.id,
-            { $pull: { following: req.body.idToUnfollow } },
-            { new: true, upsert: true },
-            (err, docs) => {
-                if (!err) res.status(201).json(docs);
-                else return res.status(400).json(err);
-            }
-        );
-        // remove to following list
-        await UserModel.findByIdAndUpdate(
-            req.body.idToUnfollow,
-            { $pull: { followers: req.params.id } },
-            { new: true, upsert: true },
-            (err, docs) => {
-                // if(!err) res.status(201).json(docs);
-                if (err) return res.status(400).json(err);
-            }
-        )
     } catch (err) {
         return res.status(500).json({ message: err });
     }
