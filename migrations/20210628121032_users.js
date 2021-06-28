c4914721d6170625c1c97eea20d1ac030278e3c6
Jessy BREYNE <jessybreyne@protonmail.com>
2021-06-28 15:25:45 +0200

    Renommage table user -> users

--- migrations/20210628121032_users.js
+++ migrations/20210628121032_users.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex) {
- return knex.schema.createTable('user', function (table) {
+ return knex.schema.createTable('users', function (table) {
     table.increments('id').primary();
     table.string('firstName').notNullable();
     table.string('lastName');
@@ -11,5 +11,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('user');
+  return knex.schema.dropTable('users');
 };
